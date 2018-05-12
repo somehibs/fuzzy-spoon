c62c5a2bf07cee9c4549f6b3962ee9e87b6c932e
Alex Brown <alex.brown@thefloow.com>
2018-05-12 00:01:10 +0000

    added dht11 lib

--- fuzzy.go
+++ fuzzy.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"github.com/michaelfranzl/bmp180"
+	"github.com/d2r2/go-dht"
 	"log"
 	"fmt"
 	"golang.org/x/exp/io/i2c"
 )
 
 func main() {
+	barometric()
+	humid()
+}
+
+func humid() {
+	temperature, humidity, retried, err :=
+		dht.ReadDHTxxWithRetry(dht.DHT11, 17, false, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print temperature and humidity
+	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		temperature, humidity, retried)
+}
+
+func barometric() {
 	// Fetch barometric pressure, output it for now
 	fs := i2c.Devfs{Dev: "/dev/i2c-1"}
 	device, err := i2c.Open(&fs, 0x77)
